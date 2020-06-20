e1cd365a53f87424639c205e7e8a1f8d38dcb7dc
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-20 11:47:14 -0300

    initial projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "./styles/global.css";
+
+import OceanWavesBeach from "./components/Audios/sounds/OceanWavesBeach";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <OceanWavesBeach />
+    </div>
+  );
 }
 
 export default App;
