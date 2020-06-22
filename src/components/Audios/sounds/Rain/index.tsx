5f9d6d6cc3ca3956fa63c281c973692745ced7d4
mateusfg7 <mateusfelipefg77@gmail.com>
2020-06-22 14:33:28 -0300

    'Rain' creation

---
+++ src/components/Audios/sounds/Rain/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import changeStateOfAudio from "../../../../functions/changeStateOfAudio";
+
+import VolumeController from "../../../../Controllers/VolumeController";
+
+import "./style.css";
+
+export default function Rain() {
+  const [rainState, setRainState] = useState(false);
+
+  return (
+    <div className='sound-component'>
+      <audio loop>
+        <source src='sounds/rain.mp3' type='audio/mp3' />
+      </audio>
+      <div
+        className='sound-button'
+        id='rain-button'
+        onClick={() => {
+          const audio = document.getElementsByTagName("audio")[4];
+          const button = document.getElementById("rain-button");
+          const audioController = document.getElementsByClassName(
+            "audio-controller"
+          )[4];
+          changeStateOfAudio(
+            audio,
+            rainState,
+            setRainState,
+            button,
+            audioController
+          );
+        }}
+      />
+      <VolumeController
+        audioObject={document.getElementsByTagName("audio")[4]}
+      />
+    </div>
+  );
+}
