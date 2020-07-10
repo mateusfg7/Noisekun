2f9caacc09daa6318d5b0d1cecccaba7394afc86
mateusfg7 <mateusfelipefg77@gmail.com>
2020-07-10 09:27:28 -0300

    import updates in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import React from "react";
 
+import * as environment from "./services/defaultVariables";
+
 import "./styles/effects.css";
 import "./styles/global.css";
 
 import Header from "./components/Header";
 import Audios from "./components/Audios";
+import Sound from "./components/Sound";
+import VolumeController from "./components/VolumeController";
+
+import changeStateOfAudio from "./functions/changeStateOfAudio";
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <div className='main-section'>
-        <Audios />
+        <Audios
+          Sound={Sound}
+          VolumeController={VolumeController}
+          changeStateOfAudio={changeStateOfAudio}
+          env={environment}
+        />
       </div>
     </div>
   );
