8f16825fe7426ac60a4b50f43ef6e14ef72464f5
mateusfg7 <mateusfelipefg77@gmail.com>
2020-07-20 13:33:22 -0300

    use fieldset

--- src/components/Configurations/index.tsx
+++ src/components/Configurations/index.tsx
@@ -4,25 +4,29 @@ import "./style.css";
 
 export default function Configurations() {
   const [url, setUrl] = useState("");
+
+  function changeBodyBackground() {
+    const body = document.querySelector("body");
+    if (body) {
+      body.style.backgroundImage = `url(${url})`;
+    }
+  }
+
   return (
     <div id='configurations'>
-      <input
-        type='text'
-        id='url'
-        name='url'
-        placeholder='background url'
-        onChange={(event) => setUrl(event.target.value)}
-      />
-      <button
-        onClick={() => {
-          const body = document.querySelector("body");
-          if (body) {
-            body.style.backgroundImage = `url(${url})`;
-          }
-        }}
-      >
-        change
-      </button>
+      <fieldset>
+        <legend>BACKGROUND</legend>
+        <input
+          type='text'
+          id='url'
+          name='url'
+          placeholder='background url'
+          onChange={(event) => {
+            setUrl(event.target.value);
+          }}
+        />
+        <button onClick={() => changeBodyBackground()}>change</button>
+      </fieldset>
     </div>
   );
 }
