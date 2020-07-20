ed5fc0dab50f837ecb890a039f31ffe1c5e545b2
mateusfg7 <mateusfelipefg77@gmail.com>
2020-07-20 15:06:17 -0300

    useEffect and local storage implementation

--- src/components/Configurations/index.tsx
+++ src/components/Configurations/index.tsx
@@ -1,32 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./style.css";
 
 export default function Configurations() {
-  const [url, setUrl] = useState("");
+  const [url, setUrl] = useState(localStorage.getItem("background_url") ?? "");
 
-  function changeBodyBackground() {
+  function saveConfigs() {
     const body = document.querySelector("body");
     if (body) {
       body.style.backgroundImage = `url(${url})`;
+      localStorage.setItem("background_url", url);
     }
   }
 
+  useEffect(() => {
+    saveConfigs();
+  }, []);
+
   return (
     <div id='configurations'>
-      <fieldset>
-        <legend>BACKGROUND</legend>
+      <h4>Background</h4>
+      <div className='fieldset'>
+        <span className='fieldset-title'>url</span>
         <input
           type='text'
           id='url'
           name='url'
-          placeholder='background url'
+          placeholder='png, jpeg, gif...'
           onChange={(event) => {
             setUrl(event.target.value);
           }}
         />
-        <button onClick={() => changeBodyBackground()}>change</button>
-      </fieldset>
+      </div>
+      <button onClick={() => saveConfigs()}>SAVE</button>
     </div>
   );
 }
