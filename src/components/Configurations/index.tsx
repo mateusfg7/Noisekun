0cb5b6f88a25304873f66645668630d31feba5ef
mateusfg7 <mateusfelipefg77@gmail.com>
2020-07-25 18:57:20 -0300

    update configurarions-side-bar

--- src/components/Configurations/index.tsx
+++ src/components/Configurations/index.tsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
+import BackgroundConfig from "./configs/background";
 
 import "./style.css";
 
 export default function Configurations() {
-  const [url, setUrl] = useState(localStorage.getItem("background_url") ?? "");
+  const [backgroundUrl, setBackgroundUrl] = useState(
+    localStorage.getItem("background_url") ?? ""
+  );
 
   function saveConfigs() {
     const body = document.querySelector("body");
     if (body) {
-      body.style.backgroundImage = `url(${url})`;
-      localStorage.setItem("background_url", url);
+      body.style.backgroundImage = `url(${backgroundUrl})`;
+      localStorage.setItem("background_url", backgroundUrl);
     }
   }
 
@@ -18,25 +21,16 @@ export default function Configurations() {
   }, []);
 
   return (
-    <div id='configurations'>
-      <h4>Background</h4>
-      <div className='fieldset'>
-        <div className='fieldset-content fieldset-content-title'>
-          <span>url</span>
-        </div>
-        <div className='fieldset-content fieldset-content-input'>
-          <input
-            type='text'
-            id='url'
-            name='url'
-            placeholder='png, jpeg, gif...'
-            onChange={(event) => {
-              setUrl(event.target.value);
-            }}
-          />
-        </div>
+    <>
+      <input type='checkbox' id='configButton' />
+      <label htmlFor='configButton' id='configLabel'>
+        <img src='/Noisekun/icons/config.svg' alt='Configurations' />
+      </label>
+      <div id='configurations'>
+        <BackgroundConfig setUrl={setBackgroundUrl} />
+
+        <button onClick={() => saveConfigs()}>SAVE</button>
       </div>
-      <button onClick={() => saveConfigs()}>SAVE</button>
-    </div>
+    </>
   );
 }
