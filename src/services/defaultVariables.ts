b5371fb19ce27cb7461a62507296d6a18c5c8f2c
codefactor-io <support@codefactor.io>
2020-07-27 12:27:24 +0000

    [CodeFactor] Apply fixes
    [ci skip] [skip ci]

--- src/services/defaultVariables.ts
+++ src/services/defaultVariables.ts
@@ -1,6 +1,7 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+
 dotenv.config();
 
-const HOST = "https://noisekun-server.herokuapp.com";
+const HOST = 'https://noisekun-server.herokuapp.com';
 
 export { HOST };
