d4a2ea2225e42ab518cb6a72cf9cc0fdb3a52eb1
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-31 15:41:17 -0300

    feat(theme): new theme
    add default theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,35 @@
+export default {
+  title: 'default',
+
+  colors: {
+    header: {
+      text: 'rgba(255, 255, 255, 0.7)',
+      background: 'none',
+    },
+    footer: {
+      text: 'rgba(255,255,255,1)',
+      background: 'rgba(255, 255, 255, 0.1)',
+    },
+    config: {
+      arrow_button: {
+        color: 'rgba(255, 255, 255, 0.5)',
+        hover: 'rgba(255, 255, 255, 0.7)',
+        checked: 'rgba(255, 255, 255, 1)',
+      },
+      fieldset: {
+        color: 'rgba(56, 56, 56,1)',
+        title_background: 'rgba(255, 255, 255, 0.7)',
+        input_background: 'rgba(255, 255, 255, 0.5)',
+      },
+      reset_button: {
+        color: 'rgba(0, 0, 0, 0.753)',
+        background: 'rgba(255, 255, 255, 0.5)',
+        hover: 'rgba(255, 200, 200, 0.5)',
+        active: {
+          desktop: 'rgba(255,255,255,1)',
+          mobile: 'rgba(255, 200, 200, 0.8)',
+        },
+      },
+    },
+  },
+};
