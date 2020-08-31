93445eb76186e141f4eef880d467deb99d04df1f
mateusfg7 <mateusfelipefg77@gmail.com>
2020-08-31 15:44:07 -0300

    feat(theme): theme on Header
    use themes on Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -8,13 +8,14 @@ export const LocalHeader = styled.header`
   height: 15vh;
 
   backdrop-filter: blur(2px);
+  background: ${(props) => props.theme.colors.header.background};
   box-shadow: 0 1px 10px rgba(0, 0, 0, 0.089);
 
   h1 {
     a {
       text-decoration: none;
       font-weight: 700;
-      color: rgba(255, 255, 255, 0.7);
+      color: ${(props) => props.theme.colors.header.text};
     }
   }
 `;
