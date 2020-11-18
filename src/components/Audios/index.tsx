167338cfdaa05707690817b463584cee89adffbf
mateusfg7 <mateusfelipefg77@gmail.com>
2020-11-18 10:22:35 -0300

    fix(audios): remove env from Sound component import

--- src/components/Audios/index.tsx
+++ src/components/Audios/index.tsx
@@ -6,7 +6,6 @@ export default function Audios({
   Sound,
   VolumeController,
   changeStateOfAudio,
-  env,
 }: IAudios): JSX.Element {
   return (
     <AudiosBlock>
@@ -14,91 +13,76 @@ export default function Audios({
         name="rain"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="storm"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="wind"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="water"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="ocean-waves"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="small-waves"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="forest-ambience"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="leafs"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="fire"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="night"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="coffee"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="fan"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="train"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="air-plane"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
       <Sound
         name="underwater"
         changeStateOfAudio={changeStateOfAudio}
         VolumeController={VolumeController}
-        env={env}
       />
     </AudiosBlock>
   );
