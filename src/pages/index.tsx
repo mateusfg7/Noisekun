d65c0dd42d8d6d656b64b861a3e087aa8895caa6
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-11-19 12:53:54 -0300

    NextApp - remove configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import usePersistedState from '../utils/usePersistedState';
 
 import environment from '../services/defaultVariables';
 
 import GlobalStyle from '../styles/global';
 
-import light from '../styles/themes/light';
-import dark from '../styles/themes/dark';
-
 import Header from '../components/Header';
 import Audios from '../components/Audios';
 import Sound from '../components/Sound';
-import Configurations from '../components/Configurations';
 import VolumeController from '../components/VolumeController';
 import Footer from '../components/Footer';
 
 import changeStateOfAudio from '../functions/changeStateOfAudio';
 
 function Home(): JSX.Element {
-  const [theme, setTheme] = usePersistedState('theme', light);
-
-  const toggleTheme = () => {
-    setTheme(theme.title === 'light' ? dark : light);
-  };
-
   return (
-    <ThemeProvider theme={theme}>
       <div className="App">
         <GlobalStyle />
 
@@ -39,10 +26,8 @@ function Home(): JSX.Element {
             env={environment}
           />
         </section>
-        <Configurations toggleTheme={toggleTheme} />
         <Footer />
       </div>
-    </ThemeProvider>
   );
 }
 
