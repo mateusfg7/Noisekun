a717207279b6570b986494a4083e3d3fdf2df7b0
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-12-06 11:49:48 -0300

    refactor: re-declare `changeStateOfAudio` function
    Delete `changeStateOfAudio` function and delcare directly on `Sound` component as
    `changeStateOfSound`

--- src/components/Sound/index.tsx
+++ src/components/Sound/index.tsx
@@ -2,8 +2,6 @@ import React, { useRef, useState } from 'react'
 
 import { VolumeController } from '../VolumeController'
 
-import changeStateOfAudio from '../../functions/changeStateOfAudio'
-
 import { SoundComponent, SoundButton } from './styles'
 
 export interface ISound {
@@ -29,12 +27,26 @@ export const Sound: React.FC<ISound> = ({ name }) => {
     underwater: 'icofont-swimmer'
   }
 
-  const [state, setState] = useState(false)
-  const audioRef = useRef<HTMLAudioElement>()
+  const [soundIsActive, setSoundIsActive] = useState(false)
+  const soundHTMLRef = useRef<HTMLAudioElement>()
+
+  function changeStateOfSound() {
+    const soundHTMLElement = soundHTMLRef.current
+
+    if (!soundIsActive) {
+      if (soundHTMLElement) {
+        soundHTMLElement.play()
+        setSoundIsActive(true)
+      }
+    } else if (soundHTMLElement) {
+      soundHTMLElement.pause()
+      setSoundIsActive(false)
+    }
+  }
 
   return (
     <SoundComponent>
-      <audio loop preload="true" ref={audioRef}>
+      <audio loop preload="true" ref={soundHTMLRef}>
         <source
           src={`${process.env.CDN_AUDIO_SERVER}/${name}.mp3`}
           type="audio/mp3"
@@ -43,19 +55,16 @@ export const Sound: React.FC<ISound> = ({ name }) => {
 
       <SoundButton
         id={`${name}-button`}
-        className={state ? 'selected' : ''}
-        onClick={() => {
-          const audio = audioRef.current
-          changeStateOfAudio(audio, state, setState)
-        }}
+        className={soundIsActive ? 'selected' : ''}
+        onClick={() => changeStateOfSound()}
       >
         <i className={`${icons[name]} icons`} />
       </SoundButton>
 
       <VolumeController
-        audioObject={audioRef.current}
+        audioObject={soundHTMLRef.current}
         id={`${name}-audio-controller`}
-        state={state}
+        state={soundIsActive}
       />
     </SoundComponent>
   )
