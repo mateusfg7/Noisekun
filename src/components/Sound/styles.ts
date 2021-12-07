dddfb98e4aeaaa9d3288903eaa171db2947911ca
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-12-07 11:09:22 -0300

    feat(ui): update background style of `SoundButton`
    Add a light white background when hover and select a sound, a shadow when hover, and a rounded
    border to component.

--- src/components/Sound/styles.ts
+++ src/components/Sound/styles.ts
@@ -20,18 +20,36 @@ export const SoundButton = styled.div`
 
   color: rgba(255, 255, 255, 0.5);
 
-  transition: 0.1s;
+  transition: 0.3s;
 
   &:hover {
     cursor: pointer;
     color: rgba(255, 255, 255, 0.8);
+
+    border-radius: 10%;
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.09);
   }
 
   &.selected {
     color: rgba(255, 255, 255, 1);
+
+    border-radius: 10% 10% 0 0;
+    background: rgba(255, 255, 255, 0.1);
   }
 
   .icons {
     font-size: 80px;
+  }
+
+  @media (max-width: 800px) {
+    &:hover {
+      background: none;
+      box-shadow: none;
+    }
+
+    &.selected {
+      background: none;
+    }
   }
 `
