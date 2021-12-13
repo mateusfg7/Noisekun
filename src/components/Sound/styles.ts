cd4c0c615d2d92ad3ba8487494bace1cf7754cfa
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-12-13 11:19:26 -0300

    feat(sound): import svg icons and remove old icon-fonts

--- src/components/Sound/styles.ts
+++ src/components/Sound/styles.ts
@@ -25,6 +25,9 @@ export const SoundButton = styled.div`
   &:hover {
     cursor: pointer;
     color: rgba(255, 255, 255, 0.8);
+    img {
+      opacity: 0.8;
+    }
 
     border-radius: 10%;
     background: rgba(255, 255, 255, 0.1);
@@ -33,6 +36,9 @@ export const SoundButton = styled.div`
 
   &.selected {
     color: rgba(255, 255, 255, 1);
+    img {
+      opacity: 1;
+    }
 
     border-radius: 10% 10% 0 0;
     background: rgba(255, 255, 255, 0.1);
@@ -40,6 +46,13 @@ export const SoundButton = styled.div`
 
   .icons {
     font-size: 80px;
+  }
+
+  img {
+    width: 80px;
+    filter: invert(100%) sepia(100%) saturate(0%) hue-rotate(288deg)
+      brightness(102%) contrast(102%);
+    opacity: 0.7;
   }
 
   @media (max-width: 800px) {
