4f7b42e3216b44244d29f986a470d4cc33f73abb
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-12-15 12:07:28 -0300

    feat(ui): change box-shadow style of Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,7 +9,7 @@ const LocalHeader = styled.header`
 
   backdrop-filter: blur(10px);
   background: rgba(255, 255, 255, 0.1);
-  box-shadow: 0 1px 10px rgba(0, 0, 0, 0.089);
+  box-shadow: 0px 1px 30px rgba(0, 0, 0, 0.05);
 
   h1 {
     a {
