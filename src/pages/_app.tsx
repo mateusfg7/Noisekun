041d70c31631306b37829e10b74f66d6e07b03ba
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-12-27 10:03:13 -0300

    feat(_app): include `Head` component and HTML metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
-
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+        <meta
+          name="description"
+          content="Listen combinations of ambient sounds for relaxing or getting more productive on your task!"
+        />
+        <meta
+          name="keywords"
+          content="noise, sound, ambience, relaxing, productive, noisli, noisekun"
+        />
+        <meta name="author" content="Mateus Felipe Gonçalves" />
+        <meta name="theme-color" content="#30A7C5" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
