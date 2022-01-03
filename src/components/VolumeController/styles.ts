4fa60b9aefb3c0a9587cd7b411f3a6db0a423f72
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-03 12:26:28 -0300

    refactor: use mobile-first concept on `VolumeController` component
    Remove additional styles for less than 700px screens and add new styles for bigger then 700px
    screens
    Update styles for bigger then 800px screens

--- src/components/VolumeController/styles.ts
+++ src/components/VolumeController/styles.ts
@@ -10,16 +10,21 @@ const VolumeControllerInput = styled.input<Props>`
   appearance: none;
 
   width: 100%;
-  height: 3px;
+  height: max-content;
 
-  background: rgba(255, 255, 255, 1);
+  background: rgba(255, 255, 255, 0.3);
+
+  background-image: linear-gradient(#fff, #fff);
+  background-size: ${props => props.percentValue + 3}%;
+  background-repeat: no-repeat;
 
   border-radius: 13px;
 
   cursor: pointer;
 
-  --thumb-height: 10px;
-  --thumb-width: 10px;
+  --thumb-opacity: 0;
+  --thumb-height: 15px;
+  --thumb-width: 15px;
   --thumb-color: rgba(255, 255, 255, 1);
   --thumb-border: none;
   --thumb-border-radius: 100%;
@@ -29,6 +34,8 @@ const VolumeControllerInput = styled.input<Props>`
   /* Chrome/Safari/Opera */
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
+    opacity: var(--thumb-opacity);
+
     height: var(--thumb-height);
     width: var(--thumb-width);
 
@@ -41,6 +48,8 @@ const VolumeControllerInput = styled.input<Props>`
   }
   /* Firefox */
   &::-moz-range-thumb {
+    opacity: var(--thumb-opacity);
+
     height: var(--thumb-height);
     width: var(--thumb-width);
 
@@ -53,6 +62,8 @@ const VolumeControllerInput = styled.input<Props>`
   }
   /* IE */
   &::-ms-thumb {
+    opacity: var(--thumb-opacity);
+
     height: var(--thumb-height);
     width: var(--thumb-width);
 
@@ -68,7 +79,59 @@ const VolumeControllerInput = styled.input<Props>`
     opacity: 1;
   }
 
+  @media (min-width: 700px) {
+    height: 3px;
+
+    background: rgba(255, 255, 255, 1);
+
+    background-image: none;
+
+    --thumb-opacity: 1;
+    --thumb-height: 15px;
+    --thumb-width: 15px;
+
+    /* MARKER */
+    /* Chrome/Safari/Opera */
+    &::-webkit-slider-thumb {
+      opacity: var(--thumb-opacity);
+      height: var(--thumb-height);
+      width: var(--thumb-width);
+    }
+    /* Firefox */
+    &::-moz-range-thumb {
+      opacity: var(--thumb-opacity);
+      height: var(--thumb-height);
+      width: var(--thumb-width);
+    }
+    /* IE */
+    &::-ms-thumb {
+      opacity: var(--thumb-opacity);
+
+      height: var(--thumb-height);
+      width: var(--thumb-width);
+    }
+  }
   @media (min-width: 800px) {
+    --thumb-height: 10px;
+    --thumb-width: 10px;
+
+    /* MARKER */
+    /* Chrome/Safari/Opera */
+    &::-webkit-slider-thumb {
+      height: var(--thumb-height);
+      width: var(--thumb-width);
+    }
+    /* Firefox */
+    &::-moz-range-thumb {
+      height: var(--thumb-height);
+      width: var(--thumb-width);
+    }
+    /* IE */
+    &::-ms-thumb {
+      height: var(--thumb-height);
+      width: var(--thumb-width);
+    }
+
     &:hover {
       --thumb-hover-height: 15px;
       --thumb-hover-width: 15px;
@@ -89,35 +152,6 @@ const VolumeControllerInput = styled.input<Props>`
         height: var(--thumb-hover-height);
         width: var(--thumb-hover-width);
       }
-    }
-  }
-
-  @media (max-width: 700px) {
-    height: max-content;
-
-    background: rgba(255, 255, 255, 0.3);
-
-    background-image: linear-gradient(#fff, #fff);
-    background-size: ${props => props.percentValue + 3}%;
-    background-repeat: no-repeat;
-
-    /* MARKER */
-    /* Chrome/Safari/Opera */
-    &::-webkit-slider-thumb {
-      opacity: 0;
-      height: 15px;
-    }
-
-    /* Firefox */
-    &::-moz-range-thumb {
-      opacity: 0;
-      height: 15px;
-    }
-
-    /* IE */
-    &::-ms-thumb {
-      opacity: 0;
-      height: 15px;
     }
   }
 `
