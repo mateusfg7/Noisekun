bb3b4551693a58796a51f1a50247606e9eb80a36
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-03 11:27:49 -0300

    chore(pwa): add condition to disable pwa
    Disable PWA render when in development environment

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development'
   }
 })
