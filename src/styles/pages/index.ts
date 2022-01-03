15c863e0455982aa39e52979574597862b47d417
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-03 12:42:20 -0300

    refactor: use mobile-first concept on `SoundGridLayout` component

--- src/styles/pages/index.ts
+++ src/styles/pages/index.ts
@@ -2,24 +2,24 @@ import styled from 'styled-components'
 
 export const SoundGridLayout = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: 1fr;
   gap: 30px;
 
   margin: 25px auto;
 
-  @media (max-width: 650px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-
-  @media (max-width: 550px) {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-
-  @media (max-width: 400px) {
+  @media (min-width: 350px) {
     grid-template-columns: 1fr 1fr;
   }
 
-  @media (max-width: 300px) {
-    grid-template-columns: 1fr;
+  @media (min-width: 500px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+
+  @media (min-width: 600px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media (min-width: 700px) {
+    grid-template-columns: repeat(5, 1fr);
   }
 `
