5465f2aea21c569b79ae168e7e6cabe199264455
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-04-05 11:56:16 -0300

    feat(background): update background colors

--- src/styles/effects.ts
+++ src/styles/effects.ts
@@ -3,67 +3,67 @@ import { keyframes } from 'styled-components'
 const backgroundChange = keyframes`
 
   0% {
-    background-color: #30A7C5;
+    background-color: #16528E;
   }
   5% {
-    background-color: #2980B9;
+    background-color: #004761;
   }
   10% {
-    background-color: #006B8F;
+    background-color: #0A615C;
   }
   15% {
-    background-color: #167C80;
+    background-color: #147B57;
   }
   20% {
-    background-color: #1DAF7C;
+    background-color: #226D6C;
   }
   25% {
-    background-color: #4940AA;
+    background-color: #2F5E81;
   }
   30% {
-    background-color: #DB162F;
-  }
-  35% {
-    background-color: #30A7C5;
-  }
-  40% {
-    background-color: #2980B9;
-  }
-  45% {
-    background-color: #006B8F;
-  }
-  50% {
-    background-color: #167C80;
-  }
-  55% {
-    background-color: #1DAF7C;
-  }
-  60% {
     background-color: #4940AA;
   }
+  35% {
+    background-color: #16528E;
+  }
+  40% {
+    background-color: #004761;
+  }
+  45% {
+    background-color: #0A615C;
+  }
+  50% {
+    background-color: #147B57;
+  }
+  55% {
+    background-color: #226D6C;
+  }
+  60% {
+    background-color: #2F5E81;
+  }
   65% {
-    background-color: #DB162F;
+    background-color: #4940AA;
   }
   70% {
-    background-color: #30A7C5;
+    background-color: #16528E;
   }
   75% {
-    background-color: #2980B9;
+    background-color: #004761;
   }
   80% {
-    background-color: #006B8F;
+    background-color: #0A615C;
   }
   85% {
-    background-color: #167C80;
+    background-color: #147B57;
   }
   90% {
-    background-color: #1DAF7C;
+    background-color: #226D6C;
   }
   95% {
-    background-color: #9516DF;
+    background-color: #2F5E81;
   }
   100% {
-    background-color: #30A7C5;
+    background-color: #4940AA;
   }
 `
 
