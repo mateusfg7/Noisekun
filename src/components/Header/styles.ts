7d55666b17a117d7ef4d4393872e7bee136f10a1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-04-06 11:14:33 -0300

    feat(ui): update header box-shadown and title color

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,14 +9,17 @@ const LocalHeader = styled.header`
 
   backdrop-filter: blur(10px);
   background: rgba(255, 255, 255, 0.1);
-  box-shadow: 0px 1px 30px rgba(0, 0, 0, 0.05);
 
   h1 {
     font-weight: 500;
     a {
       text-decoration: none;
-      color: rgba(255, 255, 255, 0.7);
+      color: rgba(255, 255, 255, 0.89);
     }
+  }
+
+  @media (min-width: 800px) {
+    box-shadow: 0px 1px 30px rgba(0, 0, 0, 0.05);
   }
 `
 
