da26196a69a924472a7c93ab3389927dc1740fae
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-04-12 11:45:16 -0300

    feat(viewport): remove `user-scalable="no"` parameter on viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
         <meta
           name="description"
