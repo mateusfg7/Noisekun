c84513c5a6326a6d2c355cf0fcaa67428cb3b2ce
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-08-18 19:52:50 -0300

    feat: import main css file on _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
+
+import '../styles/main.css'
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
