116db498a7b35f24ed8f996abe0d8d4337341a5e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-08-18 19:43:54 -0300

    feat(tailwind): create additional configs for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./src/**/*.{html,ts,tsx}'],
   theme: {
-    extend: {}
+    screens: {
+      'xs': '350px',
+      '2xs': '500px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      boxShadow: {
+        'header': '0px 1px 30px rgba(0, 0, 0, 0.05)',
+        'sound': '0px 0px 30px rgba(0, 0, 0, 0.04)'
+      },
+    }
   },
   plugins: []
 }
