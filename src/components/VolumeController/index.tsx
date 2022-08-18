04e75041ae5d21ab938aa0ebf6b2c5a797e52749
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-08-18 19:50:53 -0300

    feat(tailwind): convert VolumeController component
    From styled-components to tailwindcss

--- src/components/VolumeController/index.tsx
+++ src/components/VolumeController/index.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 
-import VolumeControllerInput from './styles'
-
 export interface IVolumeController {
   state: boolean
   handleSoundVolume: (volume: number) => void
@@ -14,18 +12,24 @@ export const VolumeController: React.FC<IVolumeController> = ({
   const [rangeValue, setRangeValue] = useState(1000)
 
   return (
-    <VolumeControllerInput
-      className={state ? 'selected' : ''}
-      type="range"
-      name="audio-decrement"
-      min="20"
-      max="1000"
-      value={rangeValue}
-      percentValue={(rangeValue * 100) / 1000}
-      onChange={event => {
-        setRangeValue(Number(event.target.value))
-        handleSoundVolume(Number(event.target.value) / 1000)
-      }}
-    />
+    <div className={`w-full h-max relative group ${state ? 'opacity-1' : 'opacity-0'}`}>
+      <input
+        className="slider-input"
+        type="range"
+        name="audio-decrement"
+        min="20"
+        max="1000"
+        value={rangeValue}
+        style={{
+          backgroundImage: 'linear-gradient(#fff, #fff)',
+          backgroundSize: `${(rangeValue * 100) / 1000}%`,
+          backgroundRepeat: 'no-repeat'
+        }}
+        onChange={event => {
+          setRangeValue(Number(event.target.value))
+          handleSoundVolume(Number(event.target.value) / 1000)
+        }}
+      />
+    </div>
   )
 }
