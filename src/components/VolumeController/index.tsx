82759644d50c9e5421ec815a2ea1e2451e0ee95f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-08-22 14:35:10 -0300

    feat: use `localStorage` to save volume slider position

--- src/components/VolumeController/index.tsx
+++ src/components/VolumeController/index.tsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export interface IVolumeController {
   state: boolean
+  soundNameOnLocalStorage: string
   handleSoundVolume: (volume: number) => void
 }
 
 export const VolumeController: React.FC<IVolumeController> = ({
   state,
+  soundNameOnLocalStorage,
   handleSoundVolume
 }) => {
   const [rangeValue, setRangeValue] = useState(1000)
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const storageValue = localStorage.getItem(
+        `${soundNameOnLocalStorage}-volume`
+      )
+      if (storageValue) setRangeValue(JSON.parse(storageValue) * 1000)
+    }
+  }, [])
 
   return (
     <div
