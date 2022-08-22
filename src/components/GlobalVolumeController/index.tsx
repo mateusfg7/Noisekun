6e5a730c38e90932e4f4bc77d230ef6f625a0e16
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-08-22 14:31:25 -0300

    feat: create GlobalVolumeController

---
+++ src/components/GlobalVolumeController/index.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useGlobalVolumeStore } from '../../stores/GlobalVolumeStore'
+
+export const GlobalVolumeController: React.FC = () => {
+  const MAX_VALUE = 1000
+
+  const [rangeValue, setRangeValue] = useState(MAX_VALUE)
+
+  const setGlobalVolume = useGlobalVolumeStore(state => state.setGlobalVolume)
+
+  function handleVolume(value: number) {
+    setGlobalVolume(value / MAX_VALUE)
+    setRangeValue(value)
+  }
+
+  return (
+    <div className="w-full h-max relative group">
+      <input
+        className="slider-input"
+        type="range"
+        name="audio-decrement"
+        min="0"
+        max={MAX_VALUE}
+        value={rangeValue}
+        style={{
+          backgroundImage: 'linear-gradient(#fff, #fff)',
+          backgroundSize: `${(rangeValue * 100) / MAX_VALUE}%`,
+          backgroundRepeat: 'no-repeat'
+        }}
+        onChange={event => handleVolume(Number(event.target.value))}
+      />
+    </div>
+  )
+}
