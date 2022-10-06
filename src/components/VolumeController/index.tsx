e20113d076bb5076caf25d3bcac705bfdaafdf7b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-10-06 14:00:43 -0300

    feat(a11y): improve sound volume controller
    * Add a screen-reader only span with information about sound
    * Add title on input with the state of volume

--- src/components/VolumeController/index.tsx
+++ src/components/VolumeController/index.tsx
@@ -28,11 +28,12 @@ export const VolumeController: React.FC<IVolumeController> = ({
 
   return (
     <Container isActive={isActive}>
+      <span className="sr-only">{soundName} volume controller</span>
       <input
         className="slider-input absolute top-0 left-0"
         type="range"
         name={`${soundName}-volume-controller`}
-        title={`${soundName} Volume Controller`}
+        title={`${soundName} volume in ${Number(rangeValue / 10).toFixed(1)}%`}
         min="20"
         max="1000"
         value={rangeValue}
