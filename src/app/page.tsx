c0365be7aadc1aa0bc69784245d4cb8d75bfb1cf
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-22 12:26:22 -0300

    refactor: convert `page/` to `app/`

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Header } from '@/components/Header'
+import { Sound } from '@/components/Sound'
+import { Footer } from '@/components/Footer'
+
+import soundList from '@/sounds.json'
+import { useBackgroundStore } from '@/stores/BackgroundStore'
+
+import { Container, Main, SoundContainer, SoundGrid } from '@/styles/pages'
+
+export default function Home() {
+  const background = useBackgroundStore(set => set.background)
+
+  return (
+    <Container $background={background}>
+      <Header />
+      <Main>
+        <SoundContainer>
+          <SoundGrid>
+            {soundList.map(sound => (
+              <Sound key={sound.id} soundData={sound} />
+            ))}
+          </SoundGrid>
+        </SoundContainer>
+        <Footer />
+      </Main>
+    </Container>
+  )
+}
