67b37718750d246d41982a2f4a837ea35de2d937
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-06-22 17:17:18 -0300

    refactor: use in-file tailwin classes on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,27 +6,37 @@ import { Header } from '@/components/Header'
 import { Sound } from '@/components/Sound'
 import { Footer } from '@/components/Footer'
 
-import soundList from '@/sounds.json'
 import { useBackgroundStore } from '@/stores/BackgroundStore'
-
-import { Container, Main, SoundContainer, SoundGrid } from '@/styles/pages'
+import soundList from '@/sounds.json'
 
 export default function Home() {
   const background = useBackgroundStore(set => set.background)
 
+  const backgroundClasses = {
+    transition: 'animate-background-change',
+    dark: 'bg-gray-900',
+    'room-and-rain': 'bg-lofi-rain',
+    'train-and-rain': 'bg-train-rain',
+    waterfall: 'bg-tree',
+    static: 'bg-main',
+    'camping-fire': 'bg-camping-fire '
+  }
+
   return (
-    <Container $background={background}>
+    <div
+      className={`bg-fixed bg-no-repeat bg-cover bg-center ${backgroundClasses[background]}`}
+    >
       <Header />
-      <Main>
-        <SoundContainer>
-          <SoundGrid>
+      <div className="h-[90vh] md:h-[85vh] overflow-y-scroll">
+        <div className="flex justify-center items-center min-h-[85vh] p-14">
+          <div className="grid gap-12 grid-cols-1 xs:grid-cols-2 2xs:grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6">
             {soundList.map(sound => (
               <Sound key={sound.id} soundData={sound} />
             ))}
-          </SoundGrid>
-        </SoundContainer>
+          </div>
+        </div>
         <Footer />
-      </Main>
-    </Container>
+      </div>
+    </div>
   )
 }
