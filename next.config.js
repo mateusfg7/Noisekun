7f8e8bd9e1295574a0e31647ad2b9748fcd1694f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-08-03 22:37:25 -0300

    chore(pwa): remove next-pwa config
    BREAKING CHANGE: Temporarily removes PWA support as the https://github.com/shadowwalker/next-pwa
    project is no longer being actively maintained.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
-})
-
-module.exports = withPWA({
-  reactStrictMode: true,
-})
+module.exports = {
+  reactStrictMode: true
+}
