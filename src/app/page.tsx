a029e2247cc7b0c4c7e0e12d28f855ee9d14c0b0
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-08-05 13:12:49 -0300

    feat(ui): update layout sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
   return (
     <div className={container({ background })}>
       <Header />
-      <div className="h-[90vh] overflow-y-scroll md:h-[85vh]">
-        <div className="flex min-h-[85vh] items-center justify-center p-14">
-          <div className="grid grid-cols-1 gap-12 xs:grid-cols-2 2xs:grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6">
+      <div className="h-[90vh] overflow-y-scroll md:h-[87vh]">
+        <div className="m-auto flex w-full items-center justify-center pb-24 pt-16">
+          <div className="grid h-fit w-fit grid-cols-1 gap-12 xs:grid-cols-2 2xs:grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6">
             {soundList.map(sound => (
               <Sound key={sound.id} soundData={sound} />
             ))}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,11 +6,12 @@ import { BackgroundMenu } from '@/components/background-menu'
 
 export const Header: React.FC = () => {
   return (
-    <header className="relative flex h-[10vh] items-center justify-between gap-5 px-6 shadow-header md:h-[15vh] md:justify-center">
+    <header className="left-0 right-0 top-0 flex h-[10vh] items-center justify-between gap-5 px-6 shadow-header transition-all md:h-[13vh]">
+      <div className="hidden md:block" />
       <h1 className="text-2xl font-semibold text-white/90">
         <Link href="/">Noisekun</Link>
       </h1>
-      <div className="flex items-center gap-3 md:absolute md:right-10">
+      <div className="flex items-center gap-3">
         <GlobalVolumeController />
         <BackgroundMenu />
       </div>
