259f22e6445ac08272fc9688bcff7f7e5a7bd52b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-08-07 09:04:59 -0300

    chore(next): setup next-plugin-svgr

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
+const withSvgr = require('@newhighsco/next-plugin-svgr')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true
 }
+
+module.exports = withSvgr({
+  ...nextConfig,
+  svgrOptions: {
+    dimensions: false
+  }
+})
