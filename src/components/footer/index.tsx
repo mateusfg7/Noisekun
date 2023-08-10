73e7ca8cf771099fee22e259ed6c3f2df4a4c55e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-08-10 15:39:28 -0300

    feat(ui): add version number information on footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,8 +4,10 @@ import Link from 'next/link'
 import { tv } from 'tailwind-variants'
 import { FiAward, FiGithub } from 'react-icons/fi'
 import { AiOutlineCopyrightCircle } from 'react-icons/ai'
-import { container } from './styles'
+import { container, version } from './styles'
 import { useThemeStore } from '@/stores/theme-store'
+
+import packageJson from '../../../package.json'
 
 export function Footer() {
   const Info = ({ children, className, ...props }: ComponentProps<'a'>) => {
@@ -19,45 +21,49 @@ export function Footer() {
     )
   }
 
-  const Section = ({ children }: { children: ReactNode }) => (
-    <section className="flex flex-col gap-2 sm:gap-0">{children}</section>
-  )
-
   const theme = useThemeStore(set => set.theme)
 
   return (
     <footer className={container({ theme })}>
-      <div className="w-14 overflow-hidden rounded-2xl shadow-lg">
-        <Link href="/">
-          <Image
-            src="/icons/icon-192x192.png"
-            width={192}
-            height={192}
-            alt="Noisekun"
+      <div className="flex flex-col flex-wrap items-center justify-center gap-10 sm:flex-row md:gap-14">
+        <div className="w-14 overflow-hidden rounded-2xl shadow-lg">
+          <Link href="/">
+            <Image
+              src="/icons/icon-192x192.png"
+              width={192}
+              height={192}
+              alt="Noisekun"
+              className="cursor-pointer"
+            />
+          </Link>
+        </div>
+        <section className="flex flex-col gap-2 sm:gap-0">
+          <Info
+            href="https://github.com/mateusfg7/Noisekun/blob/master/LICENSE"
             className="cursor-pointer"
-          />
-        </Link>
-      </div>
-      <Section>
-        <Info
-          href="https://github.com/mateusfg7/Noisekun/blob/master/LICENSE"
-          className="cursor-pointer"
-        >
-          <AiOutlineCopyrightCircle /> MIT License
-        </Info>
-        <Info href="https://github.com/mateusfg7">
-          by <span className="font-bold">mateusfg7</span>
-        </Info>
-      </Section>
-      <Section>
-        <Info href="https://github.com/mateusfg7/Noisekun">
-          <FiGithub /> Source
-        </Info>
+          >
+            <AiOutlineCopyrightCircle /> MIT License
+          </Info>
+          <Info href="https://github.com/mateusfg7">
+            by <span className="font-bold">mateusfg7</span>
+          </Info>
+        </section>
+        <section className="flex flex-col gap-2 sm:gap-0">
+          <Info href="https://github.com/mateusfg7/Noisekun">
+            <FiGithub /> Source
+          </Info>
 
-        <Info href="https://github.com/mateusfg7/Noisekun/#credits">
-          <FiAward /> Credits
-        </Info>
-      </Section>
+          <Info href="https://github.com/mateusfg7/Noisekun/#credits">
+            <FiAward /> Credits
+          </Info>
+        </section>
+      </div>
+      <a
+        href={`https://github.com/mateusfg7/Noisekun/releases/tag/${packageJson.version}`}
+        className={version({ theme })}
+      >
+        {packageJson.version}
+      </a>
     </footer>
   )
 }
--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -1,7 +1,7 @@
 import { tv } from 'tailwind-variants'
 
 export const container = tv({
-  base: /*tw:*/ 'flex flex-col flex-wrap items-center justify-center gap-10 p-11 sm:flex-row md:gap-14',
+  base: /*tw:*/ 'flex flex-col items-center gap-10 p-11',
   variants: {
     theme: {
       transition: /*tw:*/ 'text-white',
@@ -11,6 +11,24 @@ export const container = tv({
       waterfall: /*tw:*/ 'text-waterfall',
       light: /*tw:*/ 'text-light-foreground',
       'camping-fire': /*tw:*/ 'text-camping-fire'
+    }
+  }
+})
+
+export const version = tv({
+  variants: {
+    theme: {
+      transition: /*tw:*/ 'text-white/60 hover:text-white active:text-white',
+      dark: /*tw:*/ 'text-dark-foreground/60 hover:text-dark-foreground active:text-dark-foreground',
+      'blue-room':
+        /*tw:*/ 'text-blue-room/60 hover:text-blue-room active:text-blue-room',
+      train: /*tw:*/ 'text-train/60 hover:text-train active:text-train',
+      waterfall:
+        /*tw:*/ 'text-waterfall/60 hover:text-waterfall active:text-waterfall',
+      light:
+        /*tw:*/ 'text-light-foreground/60 hover:text-light-foreground active:text-light-foreground',
+      'camping-fire':
+        /*tw:*/ 'text-camping-fire/60 hover:text-camping-fire active:text-camping-fire'
     }
   }
 })
