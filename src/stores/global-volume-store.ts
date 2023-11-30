ca59c41c3a2507c9b61af88d7c293d7064cadbb6
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-11-30 12:12:53 +0000

    test: add tests for `src/components/header/global-volume-controller/index.tsx`

--- src/stores/global-volume-store.ts
+++ src/stores/global-volume-store.ts
@@ -3,10 +3,12 @@ import { create } from 'zustand'
 interface GlobalVolumeState {
   globalVolume: number
   setGlobalVolume: (newGlobalVolume: number) => void
+  resetStore: () => void
 }
 
 export const useGlobalVolumeStore = create<GlobalVolumeState>(set => ({
   globalVolume: 1,
   setGlobalVolume: (newGlobalVolume: number) =>
-    set(() => ({ globalVolume: newGlobalVolume }))
+    set(() => ({ globalVolume: newGlobalVolume })),
+  resetStore: () => set(() => ({ globalVolume: 1 }))
 }))
