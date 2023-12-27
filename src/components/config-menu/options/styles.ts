c83f4d77204200e51a1445019bfef6929804a171
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2023-12-27 14:00:33 +0000

    feat(ui): improve config-menu styles

--- src/components/config-menu/options/styles.ts
+++ src/components/config-menu/options/styles.ts
@@ -4,15 +4,17 @@ export const input = tv({
   base: /*tw:*/ 'form-input w-24 h-full rounded-lg p-2 text-center bg-transparent',
   variants: {
     theme: {
-      transition: /*tw:*/ 'focus:border-main focus:ring-main',
-      dark: /*tw:*/ 'focus:border-dark-background focus:ring-dark-background',
+      transition: /*tw:*/ 'border-main/20 focus:border-main focus:ring-main',
+      dark: /*tw:*/ 'border-dark-background/20 focus:border-dark-background focus:ring-dark-background',
       light:
-        /*tw:*/ 'focus:border-light-foreground focus:ring-light-foreground',
-      'blue-room': /*tw:*/ 'focus:border-blue-room focus:ring-blue-room',
-      train: /*tw:*/ 'focus:border-train focus:ring-train',
-      waterfall: /*tw:*/ 'focus:border-waterfall focus:ring-waterfall',
+        /*tw:*/ 'border-light-foreground/20 focus:border-light-foreground focus:ring-light-foreground',
+      'blue-room':
+        /*tw:*/ 'border-blue-room/50 focus:border-blue-room focus:ring-blue-room',
+      train: /*tw:*/ 'border-train/50 focus:border-train focus:ring-train',
+      waterfall:
+        /*tw:*/ 'border-waterfall/70 focus:border-waterfall focus:ring-waterfall',
       'camping-fire':
-        /*tw:*/ 'focus:border-camping-fire focus:ring-camping-fire'
+        /*tw:*/ 'border-camping-fire/70 focus:border-camping-fire focus:ring-camping-fire'
     }
   }
 })
@@ -21,13 +23,13 @@ export const settingRow = tv({
   base: /*tw:*/ 'flex items-center justify-between rounded-xl bg-neutral-600/10 p-1 text-lg',
   variants: {
     theme: {
-      transition: /*tw:*/ 'bg-main/5',
-      dark: /*tw:*/ 'bg-dark-background/5',
-      light: /*tw:*/ 'bg-light-background',
-      'blue-room': /*tw:*/ 'bg-blue-room/10',
-      train: /*tw:*/ 'bg-train/10',
-      waterfall: /*tw:*/ 'bg-waterfall/10',
-      'camping-fire': /*tw:*/ 'bg-camping-fire/10'
+      transition: /*tw:*/ 'bg-main/0',
+      dark: /*tw:*/ 'bg-dark-background/0',
+      light: /*tw:*/ 'bg-light-background/0',
+      'blue-room': /*tw:*/ 'bg-blue-room/0',
+      train: /*tw:*/ 'bg-train/0',
+      waterfall: /*tw:*/ 'bg-waterfall/0',
+      'camping-fire': /*tw:*/ 'bg-camping-fire/0'
     }
   }
 })
