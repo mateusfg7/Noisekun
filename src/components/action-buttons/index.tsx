f2dc7a0620fac778c47d5443b82c1a0df86388ba
hemanth110702 <mamidihemanthkumar2002@gmail.com>
2026-04-11 23:53:51 +0530

    feat: centralized sound icons with flex-layout and flex wrap instead of grid layout

--- src/components/action-buttons/index.tsx
+++ src/components/action-buttons/index.tsx
@@ -6,7 +6,7 @@ export function ActionButtons() {
   return (
     <div
       data-testid="action-buttons"
-      className="hidden w-full items-center justify-end gap-2 px-4 xs:flex"
+      className="hidden w-full items-center justify-end gap-2 pr-14 xs:flex max-[500px]:justify-center max-[500px]:pr-0"
     >
       <SaveCombo />
       <Share />
